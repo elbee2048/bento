6312e1b9d830f41e363a7153aee29d9668191682
elbee2048 <56700577+elbee2048@users.noreply.github.com>
2026-07-12 17:03:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '33.3828',
+	defaultLongitude: '-84.07873',
 
 	// Autochange
 	autoChangeTheme: true,
